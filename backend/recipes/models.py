94b846105cbc43b72062de7bf7cc1286a9169a29
Yurij Nazarov <naz4rov96@yandex.ru>
2023-08-26 14:22:26 +0300

    add recipes models

---
+++ backend/recipes/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200)
+    measurement_unit = models.CharField(max_length=200)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    color = models.CharField(max_length=7, null=True)
+    slug = models.SlugField(max_length=200)
+
+
+class Recipy(models.Model):
+    name = models.CharField(max_length=200)
+    author = models.ForeignKey(User, related_name='recipes',
+                               on_delete=models.CASCADE)
+    text = models.TextField()
+    cooking_time = models.SmallIntegerField()  # добавиьб валицацию >= 1
+    image = models.ImageField(upload_to='recipes/images/')
+    tags = models.ManyToManyField(Tag, through='TagRecipy')
+    ingredients = models.ManyToManyField(Ingredient,
+                                         through='IngredientRecipy')
+
+
+class TagRecipy(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    recipy = models.ForeignKey(Recipy, on_delete=models.CASCADE)
+
+
+class IngredientRecipy(models.Model):
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    recipy = models.ForeignKey(Recipy, on_delete=models.CASCADE)
+    quantity = models.SmallIntegerField()  # валидация
