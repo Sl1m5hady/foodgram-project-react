e239d8ae82f217d91f319a4a7a1747eebeaf1f74
Yurij Nazarov <naz4rov96@yandex.ru>
2023-10-31 21:52:52 +0300

    urls

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -16,9 +16,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#r+3gem5sxo#m8#a-g0w(2%s%sx0+q1%@uj_+q6%gu)(upndzs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from django.conf import settings
 
 from api.views import IngredientViewSet, RecipeViewSet, TagViewSet
 from users.views import CustomUserViewSet
+from django.conf.urls.static import static
 
 router = DefaultRouter()
 router.register(r'recipes', RecipeViewSet)
@@ -15,4 +17,4 @@ router.register(r'users', CustomUserViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
